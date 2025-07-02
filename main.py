2c5d1864c503dbb0cc04e50f827228e788ac66a1
yifanguan <yfguan@umich.edu>
2025-07-02 16:40:45 -0700

    wl test code cleanup && setup ablation study code in main.py

--- main.py
+++ main.py
@@ -67,163 +67,185 @@ def evaluate(model, train_split_idx, validation_split_idx, test_split_idx):
 
     return train_acc, valid_acc, test_acc 
 
-###############################
-# hardcoded value goes here!!!!
-dataset_name = 'Cora'
-num_mp_layers = 3
-num_fl_layers = 2 # number of mlp layer
-mp_hidden_dim = 8000
-fl_hidden_dim = 512
-epsilon = 5**0.5/2
-optimizer_lr = 0.01
-# weight_decay=5e-4
-loss_func = 'CrossEntropyLoss'
-total_epoch = 300
-display_step = 50
-dropout=0
-warm_up_epoch_num = 0
-first_layer_linear=False
-batch_normalization = False
-skip_connection = False
-###############################
-fix_seed()
-dataset = Planetoid(root='data/Planetoid', name=dataset_name, transform=T.NormalizeFeatures())
-print(f'Dataset: {dataset}:')
-print('Number of graphs:', len(dataset))
-print('Number of features:', dataset.num_features)
-print('Number of classes:', dataset.num_classes)
+def run(dataset_name, num_mp_layers, num_fl_layers, mp_hidden_dim, fl_hidden_dim, epsilon, optimizer_lr, loss_func, total_epoch):
+    ###############################
+    # hardcoded value goes here!!!!
+    # dataset_name = 'Cora'
+    # num_mp_layers = 4
+    # num_fl_layers = 2 # number of mlp layer
+    # mp_hidden_dim = 8000
+    # fl_hidden_dim = 512
+    # epsilon = 5**0.5/2
+    # optimizer_lr = 0.01
+    # # weight_decay=5e-4
+    # loss_func = 'CrossEntropyLoss'
+    # total_epoch = 300
+    # display_step = 50
+    # dropout=0
+    # warm_up_epoch_num = 0
+    # first_layer_linear=False
+    # batch_normalization = False
+    # skip_connection = False
+    ###############################
+    fix_seed()
+    dataset = Planetoid(root='data/Planetoid', name=dataset_name, transform=T.NormalizeFeatures())
+    print(f'Dataset: {dataset}:')
+    print('Number of graphs:', len(dataset))
+    print('Number of features:', dataset.num_features)
+    print('Number of classes:', dataset.num_classes)
 
-data = dataset[0]  # Cora has only one graph
+    data = dataset[0]  # Cora has only one graph
 
-print(data)
-print('Number of nodes:', data.num_nodes)
-print('Number of edges:', data.num_edges)
-print('Training nodes:', data.train_mask.sum().item())
+    print(data)
+    print('Number of nodes:', data.num_nodes)
+    print('Number of edges:', data.num_edges)
+    print('Training nodes:', data.train_mask.sum().item())
 
-d = data.x.shape[1]
-c = max(data.y.max().item() + 1, data.y.shape[0])
+    d = data.x.shape[1]
+    c = max(data.y.max().item() + 1, data.y.shape[0])
 
 
-k = wl_relabel(data, 30)
-print(k)
+    k = wl_relabel(data, 30)
+    print(k)
 
-# 先算 distinct klog(k) distinct local structure
-# random search
-# initial_num_distinct_features = torch.unique(data.x, dim=0).float().size(0)
-# print('initial_num_distinct_features: ', initial_num_distinct_features)
-# rank_of_distinct_matrix = torch.linalg.matrix_rank(torch.unique(data.x, dim=0), tol=1e-5)
-# print('initial rank of distinct matrix: ', rank_of_distinct_matrix.item())
+    # 先算 distinct klog(k) distinct local structure
+    # random search
+    # initial_num_distinct_features = torch.unique(data.x, dim=0).float().size(0)
+    # print('initial_num_distinct_features: ', initial_num_distinct_features)
+    # rank_of_distinct_matrix = torch.linalg.matrix_rank(torch.unique(data.x, dim=0), tol=1e-5)
+    # print('initial rank of distinct matrix: ', rank_of_distinct_matrix.item())
 
-model = DecoupleModel (
-    # edge_index=data.edge_index,
-    in_dim=d,
-    out_dim=c,
-    mp_width=mp_hidden_dim,
-    fl_width=fl_hidden_dim,
-    num_mp_layers = num_mp_layers,
-    num_fl_layers = num_fl_layers,
-    eps=epsilon,
-    # freeze
-    # dropout=dropout,
-    # first_layer_linear=first_layer_linear,
-    # batch_normalization=batch_normalization,
-    # skip_connection=skip_connection
-)
-# summary(model, input_data=(data))
+    model = DecoupleModel (
+        # edge_index=data.edge_index,
+        in_dim=d,
+        out_dim=c,
+        mp_width=mp_hidden_dim,
+        fl_width=fl_hidden_dim,
+        num_mp_layers = num_mp_layers,
+        num_fl_layers = num_fl_layers,
+        eps=epsilon,
+        # freeze
+        # dropout=dropout,
+        # first_layer_linear=first_layer_linear,
+        # batch_normalization=batch_normalization,
+        # skip_connection=skip_connection
+    )
+    # summary(model, input_data=(data))
 
-train_idx = torch.where(data.train_mask)[0]
-valid_idx = torch.where(data.val_mask)[0]
-test_idx = torch.where(data.test_mask)[0]
+    train_idx = torch.where(data.train_mask)[0]
+    valid_idx = torch.where(data.val_mask)[0]
+    test_idx = torch.where(data.test_mask)[0]
 
-optimizer = torch.optim.Adam(model.parameters(), lr=optimizer_lr)
-criterion = torch.nn.CrossEntropyLoss()
-if loss_func == 'CrossEntropyLoss':
+    optimizer = torch.optim.Adam(model.parameters(), lr=optimizer_lr)
     criterion = torch.nn.CrossEntropyLoss()
-elif loss_func == 'NLLLoss':
-    criterion = torch.nn.NLLLoss()
-# model.reset_parameters()
+    if loss_func == 'CrossEntropyLoss':
+        criterion = torch.nn.CrossEntropyLoss()
+    elif loss_func == 'NLLLoss':
+        criterion = torch.nn.NLLLoss()
+    # model.reset_parameters()
 
-with open('experiment_records.txt', 'a') as f:
-    f.writelines('Experiment run: \n')
-    f.writelines('dataset: ' + dataset_name + '\n')
-    f.writelines('num_mp_layers: ' + str(num_mp_layers) + '\n')
-    f.writelines('num_fl_layers: ' + str(num_fl_layers) + '\n')
-    f.writelines('mp_hidden_dim: ' + str(mp_hidden_dim) + '\n')
-    f.writelines('fl_hidden_dim: ' + str(fl_hidden_dim) + '\n')
-    f.writelines('epsilon: ' + str(epsilon) + '\n')
-    f.writelines('optimizer_lr: ' + str(optimizer_lr) + '\n')
-    # f.writelines('weight_decay: ' + str(weight_decay) + '\n')
-    f.writelines('loss_func: ' + loss_func + '\n')
-    f.writelines('total_epoch: ' + str(total_epoch) + '\n')
-    f.writelines('warm_up_epoch_num: ' + str(warm_up_epoch_num) + '\n')
-    f.writelines('first_layer_linear: ' + str(first_layer_linear) + '\n')
-    f.writelines('batch_normalization: ' + str(batch_normalization) + '\n')
-    f.writelines('skip_connection: ' + str(skip_connection) + '\n')
-    f.writelines('dropout: ' + str(dropout) + '\n')
+    with open('experiment_records.txt', 'a') as f:
+        f.writelines('Experiment run: \n')
+        f.writelines('dataset: ' + dataset_name + '\n')
+        f.writelines('num_mp_layers: ' + str(num_mp_layers) + '\n')
+        f.writelines('num_fl_layers: ' + str(num_fl_layers) + '\n')
+        f.writelines('mp_hidden_dim: ' + str(mp_hidden_dim) + '\n')
+        f.writelines('fl_hidden_dim: ' + str(fl_hidden_dim) + '\n')
+        f.writelines('epsilon: ' + str(epsilon) + '\n')
+        f.writelines('optimizer_lr: ' + str(optimizer_lr) + '\n')
+        # f.writelines('weight_decay: ' + str(weight_decay) + '\n')
+        f.writelines('loss_func: ' + loss_func + '\n')
+        f.writelines('total_epoch: ' + str(total_epoch) + '\n')
+        # f.writelines('warm_up_epoch_num: ' + str(warm_up_epoch_num) + '\n')
+        # f.writelines('first_layer_linear: ' + str(first_layer_linear) + '\n')
+        # f.writelines('batch_normalization: ' + str(batch_normalization) + '\n')
+        # f.writelines('skip_connection: ' + str(skip_connection) + '\n')
+        # f.writelines('dropout: ' + str(dropout) + '\n')
 
-loss_list = []
-best_val = float('-inf')
-best_test = float('-inf')
-train_accuracy_list = []
-valid_accuracy_list = []
-test_accuracy_list = []
+    loss_list = []
+    best_val = float('-inf')
+    best_test = float('-inf')
+    train_accuracy_list = []
+    valid_accuracy_list = []
+    test_accuracy_list = []
 
-for epoch in tqdm(range(1,total_epoch)):
-    # if epoch < warm_up_epoch_num:
-    #     model.turn_on_training()
-    # else:
-    #     model.turn_off_training()
-    loss = train()
-    loss_list.append(loss)
-    train_acc, valid_acc, test_acc = evaluate(model, train_idx, valid_idx, test_idx)
+    for epoch in tqdm(range(1,total_epoch)):
+        # if epoch < warm_up_epoch_num:
+        #     model.turn_on_training()
+        # else:
+        #     model.turn_off_training()
+        loss = train()
+        loss_list.append(loss)
+        train_acc, valid_acc, test_acc = evaluate(model, train_idx, valid_idx, test_idx)
 
-    train_accuracy_list.append(train_acc)
-    valid_accuracy_list.append(valid_acc)
-    test_accuracy_list.append(test_acc)
+        train_accuracy_list.append(train_acc)
+        valid_accuracy_list.append(valid_acc)
+        test_accuracy_list.append(test_acc)
 
-    if test_acc > best_test:
-        best_test = test_acc
-    if valid_acc > best_val:
-        best_val = valid_acc
+        if test_acc > best_test:
+            best_test = test_acc
+        if valid_acc > best_val:
+            best_val = valid_acc
 
-    if epoch % display_step == 0:
-        print(f'Epoch: {epoch:02d}, '
-              f'Loss: {loss:.4f}, '
-              f'Train: {100 * train_acc:.2f}%, '
-              f'Valid: {100 * valid_acc:.2f}%, '
-              f'Test: {100 * test_acc:.2f}%, '
-              f'Best Valid: {100 * best_val:.2f}%, '
-              f'Best Test: {100 * best_test:.2f}%')
+        if epoch % display_step == 0:
+            print(f'Epoch: {epoch:02d}, '
+                f'Loss: {loss:.4f}, '
+                f'Train: {100 * train_acc:.2f}%, '
+                f'Valid: {100 * valid_acc:.2f}%, '
+                f'Test: {100 * test_acc:.2f}%, '
+                f'Best Valid: {100 * best_val:.2f}%, '
+                f'Best Test: {100 * best_test:.2f}%')
 
-with open('experiment_records.txt', 'a') as f:
-    json.dump(train_accuracy_list, f)
-    f.write("\n")
-    json.dump(valid_accuracy_list, f)
-    f.write("\n")
-    json.dump(test_accuracy_list, f)
-    f.write("\n")
-    f.write('best Valid: ' + str(best_val) + '\n')
-    f.write('best test: ' + str(best_test) + '\n')
-    f.write("\n\n")
+    with open('experiment_records.txt', 'a') as f:
+        json.dump(train_accuracy_list, f)
+        f.write("\n")
+        json.dump(valid_accuracy_list, f)
+        f.write("\n")
+        json.dump(test_accuracy_list, f)
+        f.write("\n")
+        f.write('best Valid: ' + str(best_val) + '\n')
+        f.write('best test: ' + str(best_test) + '\n')
+        f.write("\n\n")
 
-# Plotting the loss, min cell is 0.1, large figure
-plt.figure(figsize=(10, 5))
-plt.plot(loss_list, label='Loss', color='red')
+    # Plotting the loss, min cell is 0.1, large figure
+    plt.figure(figsize=(10, 5))
+    plt.plot(loss_list, label='Loss', color='red')
 
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Loss vs Epochs')
-plt.legend()
-plt.savefig('loss_cora.png')
-plt.clf()  # Clear the current figure for the next plot
-# Plotting the acc in one figure
-plt.figure(figsize=(10, 5))
-plt.plot(train_accuracy_list, label='Train Accuracy', color='blue')
-plt.plot(valid_accuracy_list, label='Valid Accuracy', color='orange')
-plt.plot(test_accuracy_list, label='Test Accuracy', color='green')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.title('Accuracy vs Epochs')
-plt.legend()
-plt.savefig('accuracy_cora.png')
-plt.clf()  # Clear the current figure for the next plot
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Loss vs Epochs')
+    plt.legend()
+    plt.savefig('loss_cora.png')
+    plt.clf()  # Clear the current figure for the next plot
+    # Plotting the acc in one figure
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_accuracy_list, label='Train Accuracy', color='blue')
+    plt.plot(valid_accuracy_list, label='Valid Accuracy', color='orange')
+    plt.plot(test_accuracy_list, label='Test Accuracy', color='green')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy vs Epochs')
+    plt.legend()
+    plt.savefig('accuracy_cora.png')
+    plt.clf()  # Clear the current figure for the next plot
+
+
+def ablation_study():
+    ###############################
+    # Experiment setup
+    dataset_name = 'Cora'
+    num_mp_layers = 4
+    num_fl_layers = 2 # number of mlp layer
+    mp_hidden_dim = 8000
+    fl_hidden_dim = 512
+    epsilon = 5**0.5/2
+    optimizer_lr = 0.01
+    # weight_decay=5e-4
+    loss_func = 'CrossEntropyLoss'
+    total_epoch = 300
+    ###############################
+    run(dataset_name, num_mp_layers, num_fl_layers, mp_hidden_dim, fl_hidden_dim, epsilon, optimizer_lr, loss_func, total_epoch)
+
+
+
+ablation_study()
--- utils/wl_test.py
+++ utils/wl_test.py
@@ -2,9 +2,7 @@
 import networkx as nx
 from hashlib import md5
 from collections import Counter
-import importlib.resources
-import struct
-from torch_geometric.utils import to_networkx, from_networkx
+from torch_geometric.utils import to_networkx
 from torch_geometric.data import Data
 
 
@@ -12,10 +10,7 @@ def wl_hash(node_label, neighbor_labels):
     # Sort neighbor labels and concatenate with node label
     combined = str(node_label) + "|".join(sorted(map(str, neighbor_labels)))
     hash_str = md5(combined.encode()).hexdigest()
-    # hash_int = int(hash_str, 16)
-    # # Normalize to [0, 1]
-    # max_int = int("f" * 32, 16)  # max possible md5 value = 2^128 - 1
-    # normalized = hash_int / max_int
+
     return hash_str
 
 # Reassign labels for nodes, only change old label to new label for new distinct nodes
@@ -75,9 +70,6 @@ def wl_relabel(graph: Data, h: int):
     labels = {} # node -> label
     for node in graph.nodes():
         labels[node] = graph.degree[node]
-
-    # gloabl_labels = labels.copy()
-    # print(labels)
     
     is_converge = False
     k = count_distinct(labels)
@@ -87,189 +79,15 @@ def wl_relabel(graph: Data, h: int):
         for node in graph.nodes():
             new_label = wl_hash(labels[node], [labels[neighbor] for neighbor in graph.neighbors(node)])
             new_labels[node] = new_label
-        # new_k = count_distinct(new_labels)
         new_labels = reassign_label(labels, new_labels)
         new_k = count_distinct(new_labels)
-        # print("new_k: {}".format(new_k))
-        # print("new_k_2: {}".format(new_k_2))
-        # print("new_k: {}, new_k_2: {}".format(new_k, new_k_2))
 
         if new_k == k:
             print("Converge on {} distinct structures".format(k))
             is_converge = True
 
         labels = new_labels
-        # gloabl_labels.update(labels)
         k = new_k
         print("After Iteration {}, {} distinct structures, is converge? {}".format(i, k, is_converge))
 
     return k
-
-# def wl_test(G1, G2, h: int, nodes1=None, nodes2=None) -> bool:
-#     """
-#     Perform the Weisfeiler-Lehman Test for graph isomorphism.
-#     Args:
-#         G1: A NetworkX graph with or without node labels.
-#         G2: A NetworkX graph with or without node labels.
-#         nodes: initial nodes' states i.e. initial features
-
-#     Returns:
-#         True if the graphs are not isomorphic, False if they might be isomorphic.
-#     """
-#     # initialize nodes features
-
-#     # perform testing
-#     g1_color, _ = wl_relabel(G1, nodes1, use_degree=False, h=h)
-#     g2_color, _ = wl_relabel(G2, nodes2, use_degree=False, h=h)
-#     return g1_color == g2_color
-
-
-
-# https://github.com/networkx/networkx/blob/main/networkx/algorithms/isomorphism/tests/test_isomorphism.py
-def build_graph():
-    G1 = nx.Graph()
-    G2 = nx.Graph()
-    G3 = nx.Graph()
-    G4 = nx.Graph()
-    G5 = nx.Graph()
-    G6 = nx.Graph()
-    G1.add_edges_from([[1, 2], [1, 3], [1, 5], [2, 3]])
-    G2.add_edges_from([[10, 20], [20, 30], [10, 30], [10, 50]])
-    G3.add_edges_from([[1, 2], [1, 3], [1, 5], [2, 5]])
-    G4.add_edges_from([[1, 2], [1, 3], [1, 5], [2, 4]])
-    G5.add_edges_from([[1, 2], [1, 3]])
-    G6.add_edges_from([[10, 20], [20, 30], [10, 30], [10, 50], [20, 50]])
-
-    return G1, G2
-
-class TestWikipediaExample:
-    # Source: https://en.wikipedia.org/wiki/Graph_isomorphism
-
-    # Nodes 'a', 'b', 'c' and 'd' form a column.
-    # Nodes 'g', 'h', 'i' and 'j' form a column.
-    g1edges = [
-        ["a", "g"],
-        ["a", "h"],
-        ["a", "i"],
-        ["b", "g"],
-        ["b", "h"],
-        ["b", "j"],
-        ["c", "g"],
-        ["c", "i"],
-        ["c", "j"],
-        ["d", "h"],
-        ["d", "i"],
-        ["d", "j"],
-    ]
-
-    # Nodes 1,2,3,4 form the clockwise corners of a large square.
-    # Nodes 5,6,7,8 form the clockwise corners of a small square
-    g2edges = [
-        [1, 2],
-        [2, 3],
-        [3, 4],
-        [4, 1],
-        [5, 6],
-        [6, 7],
-        [7, 8],
-        [8, 5],
-        [1, 5],
-        [2, 6],
-        [3, 7],
-        [4, 8],
-    ]
-
-    def test_graph(self):
-        g1 = nx.Graph()
-        g2 = nx.Graph()
-        g1.add_edges_from(self.g1edges)
-        g2.add_edges_from(self.g2edges)
-
-        return g1, g2
-
-
-
-class TestVF2GraphDB:
-    # https://web.archive.org/web/20090303210205/http://amalfi.dis.unina.it/graph/db/
-
-    @staticmethod
-    def create_graph(filename):
-        """Creates a Graph instance from the filename."""
-
-        # The file is assumed to be in the format from the VF2 graph database.
-        # Each file is composed of 16-bit numbers (unsigned short int).
-        # So we will want to read 2 bytes at a time.
-
-        # We can read the number as follows:
-        #   number = struct.unpack('<H', file.read(2))
-        # This says, expect the data in little-endian encoding
-        # as an unsigned short int and unpack 2 bytes from the file.
-
-        fh = open(filename, mode="rb")
-
-        # Grab the number of nodes.
-        # Node numeration is 0-based, so the first node has index 0.
-        nodes = struct.unpack("<H", fh.read(2))[0]
-
-        graph = nx.Graph()
-        for from_node in range(nodes):
-            # Get the number of edges.
-            edges = struct.unpack("<H", fh.read(2))[0]
-            for edge in range(edges):
-                # Get the terminal node.
-                to_node = struct.unpack("<H", fh.read(2))[0]
-                graph.add_edge(from_node, to_node)
-
-        fh.close()
-        return graph
-
-    def test_graph(self):
-        head = importlib.resources.files("networkx.algorithms.isomorphism.tests")
-        g1 = self.create_graph(head / "iso_r01_s80.A99")
-        g2 = self.create_graph(head / "iso_r01_s80.B99")
-        return g1, g2
-
-if __name__ == "__main__":
-    # Build graph
-    # Create an empty graph
-    # G1 = nx.Graph()
-
-    # # Add nodes and edges
-    # G1.add_nodes_from([1, 2, 3])
-    # G1.add_edges_from([(1, 2), (2, 3), (3, 1)])
-
-    # View nodes and edges
-    # print(G1.nodes())  # [1, 2, 3]
-    # print(G1.edges())  # [(1, 2), (2, 3), (3, 1)]
-
-    # Create two isomorphic graphs (triangle)
-    G1 = nx.Graph()
-    G1.add_edges_from([(0, 1), (1, 2), (2, 0)])
-
-    # G2 = nx.Graph()
-    # G2.add_edges_from([(10, 11), (11, 12), (12, 10)])
-
-
-    # G1, G2 = TestVF2GraphDB().test_graph()
-    # Run WL test
-    # (1) use node degree
-    # labels1, h1 = wl_relabel(G1, None, True, h=3)
-    # labels2, h2 = wl_relabel(G2, None, True, h=3)
-
-    # (2) use intial node embedding
-    # nodes1 = {}
-    # for node in G1.nodes():
-    #     nodes1[node] = 0
-    # nodes2 = {}
-    # labels1, h1 = wl_relabel(G1, nodes1, False, h=3)
-    # labels2, h2 = wl_relabel(G2, None, True, h=3)
-
-    G1 = nx.Graph()
-    G1.add_edges_from([(0, 1), (0, 2), (1, 2), (1,3), (2,3)])
-    wl_relabel(from_networkx(G1), 10)
-
-
-    # print("G1 WL Histogram:", h1)
-    # print("G2 WL Histogram:", h2)
-    # print("Are histograms equal? ", h1 == h2)
-    # print("Is G1 isomorphism to G2? {}".format(set(Counter(labels1.values()).values()) == set(Counter(labels2.values()).values())))
